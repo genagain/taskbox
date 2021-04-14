d9c44cf85d729ce8c1f21dd7a0980ff296e1b47b
Gen Ohta <genohta@Gens-MacBook-Pro.local>
2021-04-13 17:22:59 -0700

    create an inbox screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { Provider } from 'react-redux';
 import store from './lib/redux';
 
-import TaskList from './components/TaskList';
+import InboxScreen from './components/InboxScreen';
+
+import './index.css';
 
 function App() {
   return (
-        <Provider store={store}>
-            <TaskList />
-          </Provider>
-        );
+    <Provider store={store}>
+      <InboxScreen />
+    </Provider>
+  );
 }
-
 export default App;
